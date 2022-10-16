86af927ecd8a1a2b57d88b64d46da1060f386041
Jackson Stafford <jackmstafford+github@gmail.com>
2022-10-16 14:21:09 -0700

    Updates

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App(): React.ReactElement {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Katherine Jelte</h3>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
